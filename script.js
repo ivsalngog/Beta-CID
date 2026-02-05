00a3041f9fcd7c2936dda3067d21da4012cbe93f
ivsalngog <ivsalngog.pn@gmail.com>
2026-02-05 09:55:50 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyVxWD4OAGLp5X84wAYxae3HJwMN66alDTXntRXrHPSPjxPFe_JENtsh3Wdad0vOChqSg/exec'
+
+const form = document.forms['register-form']
+
+document.querySelectorAll('.uppercase').forEach(input => {
+  input.addEventListener('input', () => {
+    input.value = input.value.toUpperCase();
+  });
+});
+
+form.addEventListener('submit', e => {
+	e.preventDefault()
+	fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+	.then(response => alert('Thank you! You have successfully registered the instructor.'))
+	.then(() => { window.location.reload(); })
+	.catch(error => console.error('Error!', error.message))
+})
